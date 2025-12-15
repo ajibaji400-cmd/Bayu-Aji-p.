114ff48aaad0377426d251e9846a99838c79c993
ajibaji400-cmd <ajibaji400@gmail.com>
2025-12-15 13:05:06 +0700

    Add alert functions for promotions and purchases

---
+++ script.js
@@ -0,0 +1,7 @@
+function showAlert() {
+    alert("Promo Hari Ini!\nDiskon 20% untuk semua sepatu olahraga.");
+}
+
+function buyProduct(namaProduk) {
+    alert("Anda memilih " + namaProduk + ".\nTerima kasih sudah berbelanja di SportShoes!");
+}
